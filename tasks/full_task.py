edb0dd94e4952b660c096cb70bc19dad286fa433
Georgeta-Cristina Garbacea <garbacea@umich.edu>
2025-03-21 17:04:59 -0500

    Updating paths

--- tasks/full_task.py
+++ tasks/full_task.py
@@ -84,7 +84,7 @@ class FullTask(Task):
         self.distraction_prob = distraction_prob
         self.times_perturbed = 0
         self.times_distracted = 0
-        self.wikipedia_llm = open('wikipedia_llm.txt', 'r')  # for distractions
+        self.wikipedia_llm = open('../utils/wikipedia_llm.txt', 'r')  # for distractions
 
         ## set the random seed ##
         if kwargs.get('seed', None):
