10e0489d25ec77e6ec5e0f35873542b0124bfbed
Georgeta-Cristina Garbacea <garbacea@umich.edu>
2025-04-04 20:12:06 -0500

    Typo

--- tasks/full_task.py
+++ tasks/full_task.py
@@ -68,7 +68,7 @@ class DoneWithCondition(Done):
         elif len(self.env.ontable) == 1:
             raise ValueError(f"You are supposed to create two towers of as equal height as possible. Currently all your blocks are in just one tower, which means the towers are maximally unequal.")
         else:
-            raise ValueError(f"Your are not done. {self.env.describe_state()} So not all blocks are in just two towers.")
+            raise ValueError(f"You are not done. {self.env.describe_state()} So not all blocks are in just two towers.")
 
 
 class FullTask(Task):
